5675498d254b847bbc460ad6750224f92e248f2e
JackG <jacques.gonzalez@me.com>
2016-02-24 18:27:00 +0100

    Added topics covered.

--- Project25/ViewController.swift
+++ Project25/ViewController.swift
@@ -7,18 +7,30 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UINavigationControllerDelegate, UIImagePickerControllerDelegate, MCSessionDelegate, MCBrowserViewControllerDelegate {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
     var images = [UIImage]()
     
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var advertiserAssistant: MCAdvertiserAssistant!
+    let serviceType = "hws-project25"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Selfie Share"
+        // The string of the action parameter has no semicolon.
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Camera, target: self, action: "importPicture")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "showConnectionPrompt")
+        
+        peerID = MCPeerID(displayName: UIDevice.currentDevice().name)
+        session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .Required)
+        session.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -66,9 +78,82 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         
         images.insert(newImage, atIndex: 0)
         collectionView.reloadData()
+        
+        if session.connectedPeers.count > 0 {
+            if let imageData = UIImagePNGRepresentation(newImage) {
+                do {
+                    try session.sendData(imageData, toPeers: session.connectedPeers, withMode: .Reliable)
+                } catch let error as NSError {
+                    let alertController = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .Alert)
+                    alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                    presentViewController(alertController, animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Media Connectivity methods.
+    func showConnectionPrompt() {
+        let alertController = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .ActionSheet)
+        
+        alertController.addAction(UIAlertAction(title: "Host a session", style: .Default, handler: startHosting))
+        alertController.addAction(UIAlertAction(title: "Join a session", style: .Default, handler: joinSession))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func startHosting(action: UIAlertAction) {
+        advertiserAssistant = MCAdvertiserAssistant(serviceType: serviceType, discoveryInfo: nil, session: session)
+        advertiserAssistant.start()
+    }
+    
+    func joinSession(action: UIAlertAction) {
+        let browser = MCBrowserViewController(serviceType: serviceType, session: session)
+        browser.delegate = self
+        presentViewController(browser, animated: true, completion: nil)
+    }
+    
+    func session(session: MCSession, didReceiveStream stream: NSInputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        // Not used.
+    }
+    
+    func session(session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, withProgress progress: NSProgress) {
+        // Not used.
+    }
+    
+    func session(session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, atURL localURL: NSURL, withError error: NSError?) {
+        // Not used.
+    }
+    
+    func session(session: MCSession, peer peerID: MCPeerID, didChangeState state: MCSessionState) {
+        switch state {
+        case MCSessionState.Connected:
+            NSLog("Connected: \(peerID.displayName)")
+        case MCSessionState.Connecting:
+            NSLog("Connecting: \(peerID.displayName)")
+        case MCSessionState.NotConnected:
+            NSLog("Not connected: \(peerID.displayName)")
+        }
+    }
+    
+    func session(session: MCSession, didReceiveData data: NSData, fromPeer peerID: MCPeerID) {
+        if let image = UIImage(data: data) {
+            dispatch_async(dispatch_get_main_queue(), { [unowned self] () -> Void in
+                self.images.insert(image, atIndex: 0)
+                self.collectionView.reloadData()
+            })
+        }
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Multipeer Connectivity Framework.
 
 ## Topics covered
 
+- Setting up a collection view and image picker controller (```UICollectionViewDataSource```, ```UICollectionViewDelegate```, ```UIImagePickerControllerDelegate```).
+- Using ```MCPeerID```, ```MCSession```, ```MCAdvertiserAssistant```, ```MCBrowserViewController```.
+- Using ```do-try-catch``` with ```NSError``` and displaying a localized error message.
+- Using an ```UIAlertController``` as an ```UIActionSheet```.
